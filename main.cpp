7a577fee16535b60f4237579131af6980a0fc95f
Kyle Yeo <weekiat91@hotmail.com>
2016-02-18 19:03:00 +0800

    Added main loop

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+// Include other classes here
+// #include "Assn1.h"
+
+using namespace std;
+
+int main() {
+
+	// Create system object
+	//Assn3 assn3;
+	//assn3.main();
+
+	return 0;
+}
